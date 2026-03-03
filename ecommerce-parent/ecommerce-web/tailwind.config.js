b1c413061b8ccd82bba030dcf1efea007345a5a8
Abdelrahman Emad <abdoemad552@gmail.com>
2026-03-03 14:46:35 +0200

    Configured Tailwind CSS

---
+++ ecommerce-parent/ecommerce-web/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/main/webapp/**/*.{html,jsp,js}"
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: [],
+}
