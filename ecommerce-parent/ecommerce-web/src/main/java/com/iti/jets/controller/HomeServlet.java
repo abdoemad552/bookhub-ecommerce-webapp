4c533f40e6406472cc67eed2319e87eca097c291
Abdelrahman Emad <abdoemad552@gmail.com>
2026-03-04 21:22:30 +0200

    Initial setup for ui

--- ecommerce-parent/ecommerce-web/src/main/java/com/iti/jets/controller/HomeServlet.java
+++ ecommerce-parent/ecommerce-web/src/main/java/com/iti/jets/controller/HomeServlet.java
@@ -2,5 +2,5 @@ package com.iti.jets.controller;
 
 import jakarta.servlet.http.HttpServlet;
 
-public class LoginServlet extends HttpServlet {
+public class HomeServlet extends HttpServlet {
 }
---
+++ ecommerce-parent/ecommerce-web/src/main/java/com/iti/jets/mock/dto/BookCardDto.java
@@ -0,0 +1,8 @@
+package com.iti.jets.mock.dto;
+
+public class BookCardDto {
+    private String title;
+    private String author;
+    private double averageRating;
+    private double price;
+}
