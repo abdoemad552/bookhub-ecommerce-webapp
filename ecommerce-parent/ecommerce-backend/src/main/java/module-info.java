ae6beee6e930afc4c283b3c7cd155cdc892153de
ReemMohy259 <reem.ahmed02@eng-st.cu.edu.eg>
2026-03-08 22:15:51 +0200

    Add the lombok dependency for using their annotations (to reduce the boilerplate code)

--- ecommerce-parent/ecommerce-backend/src/main/java/module-info.java
+++ ecommerce-parent/ecommerce-backend/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ecommerce.backend {
     requires org.mapstruct;
     requires jakarta.jakartaee.api;
     requires org.hibernate.orm.core;
+    requires static lombok;
 
     exports com.iti.jets.model.dto;
     exports com.iti.jets.service;
