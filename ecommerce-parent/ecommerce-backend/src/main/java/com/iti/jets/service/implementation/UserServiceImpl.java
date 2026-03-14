d73f9dd80b12b3783076a179fbf0a8ca4b53f674
ReemMohy259 <reem.ahmed02@eng-st.cu.edu.eg>
2026-03-14 06:15:44 +0200

    Implement basic methods inside UserService

--- ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/service/implementation/UserServiceImpl.java
+++ ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/service/implementation/UserServiceImpl.java
@@ -2,15 +2,18 @@ package com.iti.jets.service.implementation;
 
 import com.iti.jets.mapper.UserMapper;
 import com.iti.jets.model.dto.response.UserDTO;
+import com.iti.jets.model.entity.User;
 import com.iti.jets.repository.interfaces.CategoryRepository;
 import com.iti.jets.repository.interfaces.UserRepository;
+import com.iti.jets.service.generic.ContextHandler;
 import com.iti.jets.service.interfaces.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Optional;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends ContextHandler implements UserService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
@@ -27,32 +30,64 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDTO findById(Long aLong) {
-        return null;
+    public UserDTO findById(Long id) {
+        return executeInContext(() -> {
+            Optional<User> userOpt = userRepository.findById(id);
+            if(userOpt.isPresent()){
+                return userMapper.toDTO(userOpt.get());
+            }else{
+                return null;
+            }
+        });
     }
 
     @Override
     public List<UserDTO> findAll() {
-        return List.of();
+        return executeInContext(() -> userRepository
+                .findAll()
+                .stream()
+                .map(userMapper::toDTO)
+                .toList()
+        );
     }
 
     @Override
     public List<UserDTO> findAll(int pageNumber, int pageSize) {
-        return List.of();
+        return executeInContext(() -> userRepository
+                .findAll(pageNumber, pageSize)
+                .stream()
+                .map(userMapper::toDTO)
+                .toList()
+        );
     }
 
     @Override
-    public void delete(Long aLong) {
-
+    public void delete(Long id) {
+        executeInContext(() -> {
+            Optional<User> userOpt = userRepository.findById(id);
+            if (userOpt.isPresent()) {
+                userRepository.delete(userOpt.get());
+            }
+        });
     }
 
     @Override
     public long count() {
-        return 0;
+        return executeInContext(userRepository::count);
     }
 
     @Override
-    public boolean existsById(Long aLong) {
-        return false;
+    public boolean existsById(Long id) {
+        return executeInContext(() -> userRepository.existsById(id));
+    }
+
+    @Override
+    public boolean existsByUserName(String username) {
+        return executeInContext(() -> userRepository.existsByUserName(username));
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return executeInContext(() -> userRepository.existsByEmail(email));
     }
 }
--- ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/service/interfaces/UserService.java
+++ ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/service/interfaces/UserService.java
@@ -4,4 +4,8 @@ import com.iti.jets.model.dto.response.UserDTO;
 import com.iti.jets.service.generic.BaseService;
 
 public interface UserService extends BaseService<UserDTO, Long> {
+
+    boolean existsByUserName(String username);
+
+    boolean existsByEmail(String email);
 }
