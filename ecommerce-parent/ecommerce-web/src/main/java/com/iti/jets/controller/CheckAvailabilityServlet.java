f7c6fa7570845c3d0eb3422e4df05022c90cd4f2
ReemMohy259 <reem.ahmed02@eng-st.cu.edu.eg>
2026-03-14 07:03:35 +0200

    Add CheckAvailabilityServlet that helps us in AJAX request to validate username and email availability

---
+++ ecommerce-parent/ecommerce-web/src/main/java/com/iti/jets/controller/CheckAvailabilityServlet.java
@@ -0,0 +1,38 @@
+package com.iti.jets.controller;
+
+import com.iti.jets.service.factory.ServiceFactory;
+import com.iti.jets.service.interfaces.UserService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/checkAvailability")
+public class CheckAvailabilityServlet extends HttpServlet {
+
+    UserService userService;
+
+    @Override
+    public void init() {
+        userService = ServiceFactory.getInstance().getUserService();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, IOException {
+        String username = req.getParameter("username");
+        String email = req.getParameter("email");
+
+        boolean available = true;
+        if (username != null) {
+            available = !userService.existsByUserName(username);
+        } else if (email != null) {
+            available = !userService.existsByEmail(email);
+        }
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("{\"available\":" + available + "}");
+    }
+}
