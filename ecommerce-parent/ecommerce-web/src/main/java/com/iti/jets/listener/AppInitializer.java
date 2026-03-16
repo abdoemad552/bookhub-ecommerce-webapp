05e49ae7b2fe7b8301c356ec66098db985029128
ReemMohy259 <reem.ahmed02@eng-st.cu.edu.eg>
2026-03-16 02:27:54 +0200

    Make only one listener that manage EMF lifecycle

--- ecommerce-parent/ecommerce-web/src/main/java/com/iti/jets/listener/AppInitializer.java
+++ ecommerce-parent/ecommerce-web/src/main/java/com/iti/jets/listener/AppInitializer.java
@@ -4,17 +4,23 @@ import com.iti.jets.config.JPAConfig;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebListener
 public class AppInitializer implements ServletContextListener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppInitializer.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        LOGGER.info("Creating Entity Manager Factory...");
         JPAConfig.getEntityManagerFactory();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        LOGGER.info("Destroying Entity Manager Factory...");
         JPAConfig.closeEntityManagerFactory();
     }
 }
