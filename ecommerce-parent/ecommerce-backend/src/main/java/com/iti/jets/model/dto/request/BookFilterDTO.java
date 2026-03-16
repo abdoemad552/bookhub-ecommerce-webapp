97a9527c552e8534f35a5d5bd21660328e75f392
Yousef Abdallah <yousefabdallah031@gmail.com>
2026-03-16 23:45:25 +0200

    Missing DTOs' details added

--- ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/model/dto/request/BookFilterDTO.java
+++ ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/model/dto/request/BookFilterDTO.java
@@ -1,5 +1,16 @@
 package com.iti.jets.model.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookFilterDTO {
     private String category;
     private double minPrice;
--- ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/model/dto/response/ReviewDTO.java
+++ ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/model/dto/response/ReviewDTO.java
@@ -1,15 +1,28 @@
 package com.iti.jets.model.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ReviewDTO {
-    private long id;
+    private Long id;
 
-    private long userId;
+    private Long userId;
     private String userFullName;
     private String userProfilePicUrl;
 
     private String comment;
-    private double averageRating;
+    private Integer rating;
     private LocalDateTime createdAt;
 }
