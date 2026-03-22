0fd4ace94a91cd2144e214c4d53d81c0ac84e7ba
ReemMohy259 <reem.ahmed02@eng-st.cu.edu.eg>
2026-03-22 05:43:39 +0200

    Refresh the user data inside the session object for every request on specific page

---
+++ ecommerce-parent/ecommerce-web/src/main/java/com/iti/jets/filter/UserRefreshFilter.java
@@ -0,0 +1,44 @@
+package com.iti.jets.filter;
+
+import com.iti.jets.model.dto.response.UserDTO;
+import com.iti.jets.service.factory.ServiceFactory;
+import com.iti.jets.service.interfaces.UserService;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter({"/checkout", "/order", "/profile"})
+public class UserRefreshFilter implements Filter {
+
+    private UserService userService;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        userService = ServiceFactory.getInstance().getUserService();
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpSession session = request.getSession(false);
+
+        if (session != null && session.getAttribute("user") != null) {
+
+            UserDTO sessionUser = (UserDTO) session.getAttribute("user");
+
+            // reload from DB
+            UserDTO freshUser = userService.findById(sessionUser.getId());
+
+            if (freshUser != null) {
+                session.setAttribute("user", freshUser);
+            }
+        }
+
+        chain.doFilter(req, res);
+    }
+}
