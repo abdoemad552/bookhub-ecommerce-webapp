b833d0430f41ac167f6edb8cb1a17694f024f9c0
ReemMohy259 <reem.ahmed02@eng-st.cu.edu.eg>
2026-03-22 20:38:20 +0200

    Implement order confirmation/details logic in backend module

--- ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/service/implementation/OrderServiceImpl.java
+++ ecommerce-parent/ecommerce-backend/src/main/java/com/iti/jets/service/implementation/OrderServiceImpl.java
@@ -1,12 +1,15 @@
 package com.iti.jets.service.implementation;
 
 import com.iti.jets.model.dto.request.PlaceOrderRequestDTO;
+import com.iti.jets.model.dto.response.OrderDTO;
+import com.iti.jets.model.dto.response.OrderItemDTO;
 import com.iti.jets.model.dto.response.factory.BaseResponse;
 import com.iti.jets.model.dto.response.factory.ResponseFactory;
 import com.iti.jets.model.entity.Book;
 import com.iti.jets.model.entity.Order;
 import com.iti.jets.model.entity.OrderItem;
 import com.iti.jets.model.entity.User;
+import com.iti.jets.model.enums.OrderStatus;
 import com.iti.jets.repository.interfaces.BookRepository;
 import com.iti.jets.repository.interfaces.CartRepository;
 import com.iti.jets.repository.interfaces.OrderRepository;
@@ -17,7 +20,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 
 public class OrderServiceImpl extends ContextHandler implements OrderService {
 
@@ -63,6 +71,132 @@ public class OrderServiceImpl extends ContextHandler implements OrderService {
             String orderCode = "ORD-" + orderEntity.getCreatedAt().getYear() + "-" + orderEntity.getId();
             return ResponseFactory.success("Order Saved Successfully", orderCode);
         });
+    }
+
+    @Override
+    public BaseResponse<OrderDTO> loadOrderDetails(Long orderId) {
+        return executeInContext(() -> {
+            Optional<Order> orderOpt = orderRepository.findById(orderId);
+
+            if (orderOpt.isEmpty()) {
+                return ResponseFactory.failure("Invalid Order ID");
+            }
+            Order order = orderOpt.get();
+
+            OrderDTO orderDto = buildOrderDTO(order);
+
+            return ResponseFactory.success("Order Details Loaded",
+                    orderDto);
+        });
+    }
+
+    @Override
+    public OrderStatus getOrderStatus(Long orderId) {
+        return executeInContext(() -> {
+            Optional<Order> orderOpt = orderRepository.findById(orderId);
+
+            if (orderOpt.isEmpty()) {
+                return OrderStatus.CANCELLED;
+            }
+            Order order = orderOpt.get();
+            return getOrderStatus(order);
+        });
+    }
+
+    @Override
+    public OrderStatus getOrderStatus(Order order) {
+        return executeInContext(() -> {
+            if (order.getStatus() == OrderStatus.CANCELLED ||
+                    order.getStatus() == OrderStatus.DELIVERED) {
+                return order.getStatus();
+            }
+
+            LocalDateTime t1 = LocalDateTime.now();
+            LocalDateTime t2 = order.getCreatedAt();
+
+            Duration duration = Duration.between(t1, t2);
+
+            if (duration.toDays() >= 5) {
+                order.setStatus(OrderStatus.DELIVERED);
+                orderRepository.update(order);
+
+                return OrderStatus.DELIVERED;
+            } else {
+                return OrderStatus.PROCESSING;
+            }
+        });
+    }
+
+    @Override
+    public BaseResponse<Void> cancelOrder(Order order) {
+        return executeInContext(() -> {
+            if (order.getStatus() == OrderStatus.CANCELLED) {
+                return ResponseFactory.failure("Order is already cancelled before");
+            }
+
+            if (order.getStatus() == OrderStatus.DELIVERED) {
+                return ResponseFactory.failure("Order is already delivered, can not be cancelled");
+            }
+
+            order.setStatus(OrderStatus.CANCELLED);
+            orderRepository.update(order);
+
+            return ResponseFactory.success("Order cancelled successfully");
+        });
+    }
+
+    @Override
+    public BaseResponse<Void> cancelOrder(Long orderId) {
+        return executeInContext(() -> {
+            Optional<Order> orderOpt = orderRepository.findById(orderId);
+
+            if (orderOpt.isEmpty()) {
+                return ResponseFactory.failure("Invalid Order");
+            }
+            Order order = orderOpt.get();
+            return cancelOrder(order);
+        });
+    }
+
+    @Override
+    public boolean isOrderOwnedByUser(Long orderId, Long userId) {
+        return executeInContext(() -> {
+            Optional<Order> orderOpt = orderRepository.findById(orderId);
+
+            if (orderOpt.isEmpty()) {
+                return false;
+            }
+            Order order = orderOpt.get();
+
+            return Objects.equals(order.getUser().getId(), userId);
+        });
+    }
+
+    // Helpers
+    private OrderDTO buildOrderDTO(Order order) {
+        Set<OrderItemDTO> items = new HashSet<>();
+        for (OrderItem item : order.getItems()) {
+            items.add(
+                    OrderItemDTO.builder()
+                            .bookId(item.getBook().getId())
+                            .bookTittle(item.getBook().getTitle())
+                            .imageUrl(item.getBook().getImageUrl())
+                            .quantity(item.getQuantity())
+                            .currentPrice(item.getCurrentPrice().doubleValue())
+                            .build()
+            );
+        }
+
+        return OrderDTO.builder()
+                .orderId(order.getId())
+                .userId(order.getUser().getId())
+                .orderCode("ORD-" + order.getCreatedAt().getYear() + "-" + order.getId())
+                .createdAt(order.getCreatedAt())
+                .orderStatus(getOrderStatus(order))
+                .totalPrice(order.getTotalPrice().doubleValue())
+                .shippingPrice(0)
+                .items(items)
+                .build();
     }
 
     private Order buildOrderEntity(PlaceOrderRequestDTO request, User user) {
